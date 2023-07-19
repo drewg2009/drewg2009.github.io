3033959e236e77c7ea0faea9a7f42ae40eb6ede2
Drew Gallagher <drewg2009@gmail.com>
2023-07-19 18:35:21 -0400

    got basic collision detection working with monkey and enemies. added basic background

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,130 @@
+"use client";
+
+import { useCallback, useEffect, useState, useMemo } from 'react';
+import styles from './page.module.css';
 import Image from 'next/image'
-import styles from './page.module.css'
+
+function Enemy(props) {
+
+  return (
+    <div style={{ left: props.x, top: props.y, position: 'absolute' }}>
+      enemy
+    </div>
+  )
+}
 
 export default function Home() {
+
+  const [x, setX] = useState(20);
+  const [y, setY] = useState();
+  const [score, setScore] = useState(0)
+  const [enemies, setEnemies] = useState([])
+  const [gameOver, setGameOver] = useState(false)
+  const [enemyDataObjects, setEnemyDataObjects] = useState([])
+  const width = 100
+  const height = 100
+
+  console.log('gameOver', gameOver)
+
+  const audio = useMemo(() => new Audio('cantTouchThis.mp3'), [])
+
+  // populate the enemies
+  useEffect(() => {
+    if (enemyDataObjects.length === 0) {
+      let newEnemyObjects = []
+      for (let i = 0; i < 3; i++) {
+        newEnemyObjects.push({
+          x: 600,
+          y: (i + 1) * 200,
+          speedX: 10 * (i+1), 
+        })
+      }
+      setEnemyDataObjects(newEnemyObjects)
+
+    }
+  }, [enemyDataObjects.length])
+
+  const isColliding = useCallback(() => {
+    for(const enemyDataObject of enemyDataObjects) {
+      // console.log('check collision', x, y, enemyDataObject)
+      if (enemyDataObject.x >= x && enemyDataObject.x < x + width && enemyDataObject.y < y + height && enemyDataObject.y > y) {
+        return true
+      }
+    }
+    return false
+
+  }, [enemyDataObjects, x, y])
+
+  const handleMove = useCallback((event) => {
+
+    if (!gameOver) {
+      if (event.clientY > 0 && event.clientY <= 400) {
+        setY(event.clientY);
+      }
+
+      if (audio.paused) {
+        audio.play()
+      }
+
+    }
+
+  }, [audio, gameOver])
+
+  // game loop interval
+
+  useEffect(() => {
+    if (!gameOver) {
+      const interval = setInterval(() => {
+        if (isColliding()) {
+          console.log('COLLIDED')
+          setGameOver(true)
+          clearInterval(interval)
+        }
+      }, 30);
+      return () => clearInterval(interval);
+    }
+  }, [gameOver, isColliding])
+
+  useEffect(() => {
+    if (!gameOver) {
+      const interval = setInterval(() => {
+        setScore(currentScore => currentScore + 1)
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [gameOver, score]);
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setEnemyDataObjects(enemyDataObjects.map(enemyDataObject => {
+        return {
+          ...enemyDataObject,
+          x: enemyDataObject.x - enemyDataObject.speedX
+        }
+      }))
+    }, 100);
+    return () => clearInterval(interval);
+  }, [enemyDataObjects]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div >
+      Score:
+      <div>
+        {score}
+      </div>
+      {
+        gameOver && <p>Game Over!</p>
+      }
+      <div onMouseMove={handleMove} className={styles.container}>
+        <Image width={width} height={height} alt='dancing monkey' src='/dancing-monkey.gif' style={{ "top": y }} className={styles.character} />
+        {
+          !gameOver && enemyDataObjects.map((enemy, i) =>
+            <Enemy key={i} x={enemy.x} y={enemy.y}/>
+          )
+        }
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
